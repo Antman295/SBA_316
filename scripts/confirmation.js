068ce83a87a453c2f8c3b84eb53e3a0bfde4b4ce
Antman295 <antjosh14@gmail.com>
2024-09-26 15:33:56 -0400

    Updates
    Fixed the confirmation page, just need to do some cleaning up of the styling and navigation of pages

--- scripts/confirmation.js
+++ scripts/confirmation.js
@@ -2,17 +2,17 @@ let going = document.getElementById('confirmGoing');
 let notGoing = document.getElementById('confirmNotGoing');
 let thankYou = document.getElementById("thankYou");
 let emailAddress = document.getElementById("email");
+let form = document.getElementById("confirm");
+let message = document.getElementById(`message`);
 
-going.addEventListener("click", () => {
-    thankYou.style.color = "green";
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (event.submitter === going) {
+        message.textContent = `Details will be send to your email address: ${emailAddress.value}. See you there! :)`
+        thankYou.style.color = "green";
+    } else if (event.submitter === notGoing) {
+        message.textContent = `Sorry you can't make it. But hopefully you can make it to the next one.`
+        thankYou.style.color = "blue";
+    }
 })
-
-notGoing.addEventListener("click", () => {
-    thankYou.style.color = "blue";
-})
-
-function messageAppear() {
-    let message = document.getElementById(`message`);
-
-    let email = emailAddress.value;
-}
