949d3c8942e57cd760da7d3d06069ee38be2b6e6
Antman295 <antjosh14@gmail.com>
2024-09-25 20:21:30 -0400

    Updates
    Added a function where user can add and delete names from a list.

--- scripts/guests.js
+++ scripts/guests.js
@@ -7,3 +7,42 @@ if(confirmation) {
     window.location.href = "../index.html"
 }
 
+const input = document.getElementById('nameInput');
+const addName = document.getElementById('addButton');
+const removeName = document.getElementById('removeButton');
+const nameList = document.getElementById('list');
+
+addName.addEventListener('click', () => {
+    const name = input.value;
+
+    if (name) {
+       const newName = document.createElement('li');
+       newName.textContent = name;
+       
+       nameList.appendChild(newName);
+
+       input.value = '';
+    } else {
+        alert("Please enter a name into the text field.");
+    }
+})
+
+removeName.addEventListener('click', () => {
+    const name = input.value;
+
+    if (name) {
+        const listOfNames = nameList.querySelectorAll('li');
+
+
+        listOfNames.forEach((item) => {
+            if (item.textContent === name) {
+                nameList.removeChild(item);
+            }
+        })
+
+        input.value = '';
+    } else {
+        alert("Please enter a name that's on the list to delete it.")
+    }
+})
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,10 +17,10 @@ if (age < 18) {
     ageNode.focus();
     message.textContent = `${nameNode.value}, you're too young! You must be 18 or older!`
 } else if (age > 100) {
+    ageNode.focus();
     message.textContent = `${nameNode.value}, congrats on being over 100, but you're too old!`
 } else {
     message.textContent = `${nameNode.value}, welcome! Please proceed to the next page!`
-    ageNode.focus();
 }
 
 }
