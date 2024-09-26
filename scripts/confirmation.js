dc742738bdf7f8b0e57bfee4c9aa64a0fa7215f6
Antman295 <antjosh14@gmail.com>
2024-09-26 14:22:45 -0400

    Updates
    Fixed the confirmation page where if the user clicks on one of the buttons the "Thank You!" changes colors.

--- scripts/confirmation.js
+++ scripts/confirmation.js
@@ -1,2 +1,18 @@
 let going = document.getElementById('confirmGoing');
 let notGoing = document.getElementById('confirmNotGoing');
+let thankYou = document.getElementById("thankYou");
+let emailAddress = document.getElementById("email");
+
+going.addEventListener("click", () => {
+    thankYou.style.color = "green";
+})
+
+notGoing.addEventListener("click", () => {
+    thankYou.style.color = "blue";
+})
+
+function messageAppear() {
+    let message = document.getElementById(`message`);
+
+    let email = emailAddress.value;
+}
